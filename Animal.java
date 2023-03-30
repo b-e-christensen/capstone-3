ea360c94933aa469bc917cfb68d7568f3b5430b5
Ben Christensen <ben.eric.christensen@gmail.com>
2023-03-30 08:23:24 -0600

    first commit/starter code

---
+++ Animal.java
@@ -0,0 +1,14 @@
+/* TODO: Create the Animal class */
+
+public class Animal extends CellItem {
+
+    public Animal(String name, int num1, int num2) {
+        this.name = name;
+        this.cost = num1 * num2;
+    }
+}
+
+
+    /* TODO: Create the Animal(String, int1, int2) constructor */
+        // Initializes CellItem 'name' with String argument
+        // Intiializes CellItem 'cost' as int * int2
